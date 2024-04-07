7c7f6a494d99b063b38edeb9acdb3ebdc527305e
Andrey <you@example.com>
2024-04-07 23:06:59 +0300

    time zonet

--- firstblog/urls.py
+++ firstblog/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('', include('blog.urls')),
+    path(r'', include('blog.urls')),
 ]
